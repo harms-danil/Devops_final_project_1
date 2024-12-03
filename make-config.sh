3dfa9b67b36fcf104cbfa2a92b199f14bf490da7
harms <harms.danil@gmail.com>
2024-12-03 23:46:27 +0300

    create make-config.sh

---
+++ make-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# First argument: Client identifier
+KEY_DIR=/etc/openvpn/clients/keys
+OUTPUT_DIR=/etc/openvpn/clients/files
+BASE_CONFIG=/etc/openvpn/clients/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-crypt>') \
+> ${OUTPUT_DIR}/${1}.ovpn
