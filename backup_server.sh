d537b6c8424f85c477e36896ef8c0cb64d7b4028
harms <harms.danil@gmail.com>
2024-12-13 21:10:35 +0300

    add iptables setting

--- backup_server.sh
+++ backup_server.sh
@@ -162,7 +162,7 @@ echo -e "\n====================\nIptables configuration \n====================\n
 iptables_add INPUT -p tcp --dport 55414 -j ACCEPT -m comment --comment urbackup_FastCGI_for_web_interface
 iptables_add INPUT -p tcp --dport 55414 -j ACCEPT -m comment --comment urbackup_HTTP_web_interface
 iptables_add INPUT -p tcp --dport 55415 -j ACCEPT -m comment --comment urbackup_Internet_clients
-iptables_add OUTPUT -p udp --dport 35623 -j ACCEPT -m comment --comment urbackup_UDP broadcasts_for_discovery
+iptables_add OUTPUT -p udp --dport 35623 -j ACCEPT -m comment --comment 'urbackup UDP broadcasts for discovery'
 iptables_add INPUT -j REJECT --reject-with icmp-host-prohibited
 iptables_add FORWARD -j REJECT --reject-with icmp-host-prohibited
 echo -e "\n====================\nSaving iptables config \n====================\n"
