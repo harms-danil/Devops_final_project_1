f5a6148f60a2a6d3adcf566d65308be9f09d2932
harms <harms.danil@gmail.com>
2024-12-16 18:22:16 +0300

    update

--- backup_server.sh
+++ backup_server.sh
@@ -159,7 +159,7 @@ set -x
 #iptables_add INPUT -p tcp --dport 35622 -j ACCEPT -m comment --comment 'urbackup UDP broadcasts for discovery'
 #iptables_add INPUT -p tcp --dport 35623 -j ACCEPT -m comment --comment 'urbackup Commands and image backups'
 
-iptables_add INPUT -p tcp --dport 55414 -j ACCEPT -m comment --comment 'urbackup FastCGI for web interface'
+iptables_add INPUT -p tcp --dport 55413 -j ACCEPT -m comment --comment 'urbackup FastCGI for web interface'
 iptables_add INPUT -p tcp --dport 55414 -j ACCEPT -m comment --comment 'urbackup HTTP web interface'
 iptables_add INPUT -p tcp --dport 55415 -j ACCEPT -m comment --comment 'urbackup Internet clients'
 iptables_add OUTPUT -p udp --dport 35623 -j ACCEPT -m comment --comment 'urbackup UDP broadcasts for discovery'
