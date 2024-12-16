b1b9a621f30ceef4a4c0597a9eeb934afee85812
harms <harms.danil@gmail.com>
2024-12-16 23:08:32 +0300

    update

--- ca-crt-key.sh
+++ ca-crt-key.sh
@@ -31,101 +31,51 @@ while true; do
 	# Create pair crt and key
 	1)
 	echo -e "\n====================\nCreate crt and key \n===================="
-	while true; do
-		read -r -n 1 -p $'\n'"Continue or Skip (c|s) " cs
-		case $cs in
-			[Cc]*)
-				if [ -f "$dest_dir"/easy-rsa/pki/private/"$server_name".key ]; then
-					echo -e "\nFile $dest_dir/easy-rsa/pki/issued/$server_name found!\nDeleting it..."
-					rm -rf "$dest_dir"/easy-rsa/pki/private/"$server_name".key
-				fi
-				if [ -f $dest_dir/easy-rsa/pki/issued/"$server_name".crt ]; then
-					echo -e "\nFile $dest_dir/easy-rsa/pki/private/$server_name found!\nDeleting it..."
-					rm -rf $dest_dir/easy-rsa/pki/issued/"$server_name".crt
-				fi
-				cd $dest_dir/easy-rsa
-				./easyrsa gen-req "$server_name" nopass
-				./easyrsa sign-req server "$server_name"
-				# remove req
-				rm $dest_dir/easy-rsa/pki/reqs/"$server_name.req"
-				echo -e "\nDONE"
-				break
-				;;
-			[Ss]*)
-				echo -e "\n"
-				break
-				;;
-			*) echo -e "\nPlease answer C or S!" ;;
-		esac
-	done
+	if [ -f "$dest_dir"/easy-rsa/pki/private/"$server_name".key ]; then
+		echo -e "\nFile $dest_dir/easy-rsa/pki/issued/$server_name found!\nDeleting it..."
+		rm -rf "$dest_dir"/easy-rsa/pki/private/"$server_name".key
+	fi
+	if [ -f $dest_dir/easy-rsa/pki/issued/"$server_name".crt ]; then
+		echo -e "\nFile $dest_dir/easy-rsa/pki/private/$server_name found!\nDeleting it..."
+		rm -rf $dest_dir/easy-rsa/pki/issued/"$server_name".crt
+	fi
+	cd $dest_dir/easy-rsa
+	./easyrsa gen-req "$server_name" nopass
+	./easyrsa sign-req server "$server_name"
+	# remove req
+	rm $dest_dir/easy-rsa/pki/reqs/"$server_name.req"
+	echo -e "\nDONE"
 	;;
 
 	# Copy ca.srt
 	2)
 	echo -e "\n====================\nCopy ca.crt\n===================="
-	while true; do
-		read -r -n 1 -p $'\n'"Continue or Skip (c|s) " cs
-		case $cs in
-			[Cc]*)
-				scp -P $port "$dest_dir"/easy-rsa/pki/ca.crt  "$server_path_ssh":~/keys
-				echo -e "\nDONE"
-				break
-				;;
-			[Ss]*)
-				echo -e "\n"
-				break
-				;;
-			*) echo -e "\nPlease answer C or S!" ;;
-		esac
-	done
+	scp -P $port "$dest_dir"/easy-rsa/pki/ca.crt  "$server_path_ssh":~/keys
+	echo -e "\nDONE"
+	break
 	;;
 
 	# Copy server certificate and key
 	3)
 	echo -e "\n====================\nCopy server certificate and key\n===================="
-	while true; do
-		read -r -n 1 -p $'\n'"Continue or Skip (c|s) " cs
-		case $cs in
-			[Cc]*)
-				echo -e "\n====================\nCopy $server_name.crt\n====================\n"
-				scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$server_name".crt "$server_path_ssh":~/keys
-				echo -e "\n====================\nCopy $server_name.key\n====================\n"
-				scp -P $port "$dest_dir"/easy-rsa/pki/private/"$server_name".key "$server_path_ssh":~/keys
-				echo -e "\nDONE\n"
-				break
-				;;
-
-			[Ss]*)
-				echo -e "\n"
-				break
-				;;
-			*) echo -e "\nPlease answer C or S!\n" ;;
-		esac
-	done
+	echo -e "\n====================\nCopy $server_name.crt\n====================\n"
+	scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$server_name".crt "$server_path_ssh":~/keys
+	echo -e "\n====================\nCopy $server_name.key\n====================\n"
+	scp -P $port "$dest_dir"/easy-rsa/pki/private/"$server_name".key "$server_path_ssh":~/keys
+	echo -e "\nDONE\n"
+	break
 	;;
 
 	# Copy client certificate and key
 	4)
 	echo -e "\n====================\nCopy client certificate and key\n===================="
-	while true; do
-		read -r -n 1 -p $'\n'"Continue or Skip (c|s) " cs
-		case $cs in
-			[Cc]*)
-				read -r -e -p $'\n'"\nEnter the name of the client for which the certificate was issued: " client_name
-				echo -e "\n====================\nCopy $client_name.crt\n===================="
-				scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$client_name".crt "$server_path_ssh":~/keys
-				echo -e "\n====================\nCopy $client_name.key\n===================="
-				scp -P $port "$dest_dir"/easy-rsa/pki/private/"$client_name".key "$server_path_ssh":~/keys
-				echo -e "\nDONE"
-				break
-				;;
-			[Ss]*)
-				echo -e "\n"
-				break
-				;;
-			*) echo -e "\nPlease answer C or S!" ;;
-		esac
-	done
+	read -r -e -p $'\n'"\nEnter the name of the client for which the certificate was issued: " client_name
+	echo -e "\n====================\nCopy $client_name.crt\n===================="
+	scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$client_name".crt "$server_path_ssh":~/keys
+	echo -e "\n====================\nCopy $client_name.key\n===================="
+	scp -P $port "$dest_dir"/easy-rsa/pki/private/"$client_name".key "$server_path_ssh":~/keys
+	echo -e "\nDONE"
+	break
 	;;
 
 	# exit
