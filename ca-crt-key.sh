4c732b678f6d498b1d9747dd2054d04898194377
harms <harms.danil@gmail.com>
2024-12-24 21:09:48 +0300

    update

--- ca-crt-key.sh
+++ ca-crt-key.sh
@@ -10,17 +10,19 @@ dest_dir="/home/harms"
 username="harms"
 
 # Request the server part where to copy the keys and certificate
-read -r -e -p "Enter the SUB_name server for path (format: username@'SUB'.hostname): " sub_name
-server_path_ssh="$username@$sub_name.$host"
-server_name="$sub_name.$host"
-echo -e "\n$server_name"
-echo -e "\n$server_path_ssh"
+sub_name_request() {
+	read -r -e -p "Enter the SUB_name server for path (format: username@'SUB'.hostname): " sub_name
+	server_path_ssh="$username@$sub_name.$host"
+	server_name="$sub_name.$host"
+	echo -e "\n$server_name"
+	echo -e "\n$server_path_ssh"
+}
 
 # Select actions with certificates and keys menu
 while true; do
     echo -e "\n--------------------------\n"
-    echo -e "[1] Create pair crt and key\n"
-    echo -e "[2] Copy ca.srt\n"
+    echo -e "[1] Copy ca to server\n"
+    echo -e "[2] Create server pair crt and key\n"
     echo -e "[3] Copy server certificate and key\n"
     echo -e "[4] Copy client certificate and key\n"
     echo -e "[5] Copy server certificate to monitoring server\n"
@@ -29,9 +31,19 @@ while true; do
     read -r -n 1 -p "Select an action: " certificate
 
     case $certificate in
-	# Create pair crt and key
+
+	# Copy ca.srt
 	1)
-	echo -e "\n====================\nCreate crt and key \n===================="
+	echo -e "\n====================\nCopy ca.crt\n===================="
+	sub_name_request
+	scp -P $port "$dest_dir"/easy-rsa/pki/ca.crt  "$server_path_ssh":~/keys
+	echo -e "\nDONE"
+	;;
+
+	# Create server pair crt and key
+	2)
+	sub_name_request
+	echo -e "\n====================\nCreate crt and key for $server_name\n===================="
 	if [ -f "$dest_dir"/easy-rsa/pki/private/"$server_name".key ]; then
 		echo -e "\nFile $dest_dir/easy-rsa/pki/issued/$server_name found!\nDeleting it..."
 		rm -rf "$dest_dir"/easy-rsa/pki/private/"$server_name".key
@@ -48,44 +60,51 @@ while true; do
 	echo -e "\nDONE"
 	;;
 
-	# Copy ca.srt
-	2)
-	echo -e "\n====================\nCopy ca.crt\n===================="
-	scp -P $port "$dest_dir"/easy-rsa/pki/ca.crt  "$server_path_ssh":~/keys
-	echo -e "\nDONE"
-	break
-	;;
-
 	# Copy server certificate and key
 	3)
-	echo -e "\n====================\nCopy server certificate and key\n===================="
-	echo -e "\n====================\nCopy $server_name.crt\n====================\n"
+	sub_name_request
+	echo -e "\n====================\nCopy server certificate and key on $server_name\n===================="
+	echo -e "\nCopy $server_name.crt...\n"
 	scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$server_name".crt "$server_path_ssh":~/keys
-	echo -e "\n====================\nCopy $server_name.key\n====================\n"
+	echo -e "\nCopy $server_name.key...\n"
 	scp -P $port "$dest_dir"/easy-rsa/pki/private/"$server_name".key "$server_path_ssh":~/keys
 	echo -e "\nDONE\n"
-	break
 	;;
 
-	# Copy client certificate and key
+	# Create and Copy client certificate and key
 	4)
+	read -r -e -p $'\n'"Enter the name of the client for which the certificate was issued: " client_name
+	echo -e "\n====================\nCreate client crt and key for $client_name\n===================="
+	if [ -f $dest_dir/easy-rsa/pki/issued/"$client_name".crt ]; then
+		echo -e "\nFile $dest_dir/easy-rsa/pki/issued/$client_name found!\nDeleting it..."
+		rm -rf $dest_dir/easy-rsa/pki/issued/"$client_name".crt
+	fi
+	if [ -f "$dest_dir"/easy-rsa/pki/private/"$client_name".key ]; then
+		echo -e "\nFile $dest_dir/easy-rsa/pki/private/$client_name found!\nDeleting it..."
+		rm -rf "$dest_dir"/easy-rsa/pki/private/"$client_name".key
+	fi
+	# Create client pair crt and key
+	cd $dest_dir/easy-rsa
+	./easyrsa gen-req "$client_name" nopass
+	./easyrsa sign-req client "$client_name"
+	# remove req
+	rm $dest_dir/easy-rsa/pki/reqs/"$client_name".req
+	# Copy client pair crt and key
 	echo -e "\n====================\nCopy client certificate and key\n===================="
-	read -r -e -p $'\n'"\nEnter the name of the client for which the certificate was issued: " client_name
-	echo -e "\n====================\nCopy $client_name.crt\n===================="
-	scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$client_name".crt "$server_path_ssh":~/keys
-	echo -e "\n====================\nCopy $client_name.key\n===================="
-	scp -P $port "$dest_dir"/easy-rsa/pki/private/"$client_name".key "$server_path_ssh":~/keys
+	echo -e "\nCopy $client_name.crt...\n"
+	scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$client_name".crt "$username"@openvpn."$host":~/keys
+	echo -e "\nCopy $client_name.key...\n"
+	scp -P $port "$dest_dir"/easy-rsa/pki/private/"$client_name".key "$username"@openvpn."$host":~/keys
 	echo -e "\nDONE"
-	break
 	;;
 
 	# Copy server certificate to monitoring server
 	5)
+	sub_name_request
 	echo -e "\n====================\nCopy server certificate to monitoring server\n===================="
-	echo -e "\n====================\nCopy $server_name.crt\n====================\n"
+	echo -e "\nCopy $server_name.crt\n..."
 	scp -P $port "$dest_dir"/easy-rsa/pki/issued/"$server_name".crt "$username"@monitor."$host":~/keys
 	echo -e "\nDONE\n"
-	break
 	;;
 	# exit
 	6) echo -e "\n"
